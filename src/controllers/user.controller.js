7517ae5864687158d0c0213ef8696f584475b796
siddhart <siddhart.2021ca103@mnnit.ac.in>
2024-03-05 01:22:39 +0530

    bug fixed for register-controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,8 @@ import { uplodOnCloudinary } from "../utils/Cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler (async (req,res) =>{
+
+
     // get user details from frontend
     // validation - not empty
     // check if user already exist :  username, email
@@ -16,16 +18,14 @@ const registerUser = asyncHandler (async (req,res) =>{
     // return response
 
 
-
-
     const {fullname,email,username,password} = req.body
     // console.log(email)
 
-    if([fullname,email,username,password].some(() => field?.trim() ==="" )){
+    if([fullname,email,username,password].some((field) => field?.trim() ==="" )){
         throw new ApiError(400, "All fields are required")
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or : [ { username },{ email } ]
     })
 
@@ -34,8 +34,12 @@ const registerUser = asyncHandler (async (req,res) =>{
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path
-    const coverImageLocalPath = req.files?.coverImage[0]?.path
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path
 
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.lenght > 0){
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
     if(!avatarLocalPath)
         throw new ApiError(400,"Avatar is required")
 
