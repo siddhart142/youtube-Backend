a14e7e4c37caea662ebb8f6d080ee2d64bf4ac6b
siddhart <siddhart.2021ca103@mnnit.ac.in>
2024-03-04 13:27:28 +0530

    add user and video model

--- src/app.js
+++ src/app.js
@@ -10,9 +10,9 @@ app.use(cors({
     credentials : true
 }))
 
-app.use(express.json({limit: "16kb"})) // for allowing db to accept json
-app.use(express.urlencoded({extended: true,limit : "16kb"})) // to get data from url
-app.use(express.static("public")) 
+app.use(express.json({limit: "16kb"})) // for allowing db to accept json and setting the limit to 16kb
+app.use(express.urlencoded({extended: true,limit : "16kb"})) // to get data from url and setting the limit to 16kb
+app.use(express.static("public")) // to allow files/folder to store in server itself, here public folder will have all static files
 
 
 app.use(cookieParser()) //CRUD on cookie
